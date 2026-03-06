0c809e97128d210b6dd38dc862ea2ad09fa9f811
jheinifferoliveira <jheiniffer2013@gmail.com>
2026-03-06 19:24:59 -0300

    feat(api): adiciona os serviços de assinaturas, clientes e planos e instala as documentações do swagger e scalar

--- Subscription.Api/Program.cs
+++ Subscription.Api/Program.cs
@@ -1,16 +1,27 @@
+using Scalar.AspNetCore;
 using Subscription.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddRouting(map => map.LowercaseUrls = true);
+
+builder.Services.AddEndpointsApiExplorer(); //Swagger
+builder.Services.AddSwaggerGen(); //Swagger
 
 builder.Services.AddInfraStructure(builder.Configuration);
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapScalarApiReference(option =>
+{
+    option.WithTheme(ScalarTheme.BluePlanet);
+});
 
 app.MapOpenApi();
 
