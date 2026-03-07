8575b23cf0c194244b1063b91c833ab22f2823e2
jheinifferoliveira <jheiniffer2013@gmail.com>
2026-03-06 21:29:44 -0300

    feat(api): adiciona a consulta de planos do banco de dados no endpoint.

--- Subscription.Api/Controllers/PlanosController.cs
+++ Subscription.Api/Controllers/PlanosController.cs
@@ -1,17 +1,44 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Subscription.Domain.Entities;
+using Subscription.Domain.Enums;
+using Subscription.Infra.Data.Contexts;
 
 namespace Subscription.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PlanosController : ControllerBase
+    public class PlanosController(AppDbContext context) : ControllerBase
     {
 
         [HttpGet("listar")]
         public async Task<IActionResult> ConsultarAsync(int page = 1, int pageSize = 10)
         {
-            return StatusCode(501, new { message = "Não implementado." });
+            var query = context.Set<Plano>().AsQueryable();
+
+            var total = await query.CountAsync();
+
+            var planos = await query
+                .OrderBy(p => p.Nome)
+                .Skip((page - 1) * pageSize) //página a partir do qual iremos começar 
+                .Take(pageSize) // total de registros
+                .Select(p => new
+                {
+                    id = p.Id,
+                    nome = p.Nome.ToUpper(),
+                    valorMensal = p.ValorMensal,
+                    periodicidade = p.Periodicidade
+                })
+                .ToListAsync();
+
+            return Ok(new
+            {
+                page,
+                pageSize,
+                total,
+                data = planos
+            });
         }
     }
 }
